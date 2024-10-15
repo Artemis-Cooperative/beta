6c1e569929d5b8aec4e9b2c40ef6a1fd1b1d61e6
Ryan Ogden <12yanogden@gmail.com>
2024-10-15 16:53:23 -0400

    add dependencies

--- beta.go
+++ beta.go
@@ -1,7 +1,17 @@
 package beta
 
-import "fmt"
+import (
+	"fmt"
 
-func Run() {
+	alpha "github.com/Artemis-Cooperative/alpha"
+	packages "github.com/Artemis-Cooperative/interfaces/packages"
+)
+
+var alphaInstance packages.Alpha = alpha.Alpha{}
+
+type Beta struct{}
+
+func (b Beta) Run() {
 	fmt.Println("beta")
+	alphaInstance.Run()
 }
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/Artemis-Cooperative/beta
+
+go 1.23.0
+
+require (
+	github.com/Artemis-Cooperative/alpha v0.0.0-20241015200556-1646b92f8d9b // indirect
+	github.com/Artemis-Cooperative/interfaces v0.0.0-20241015200816-8f159be97a9c // indirect
+)
